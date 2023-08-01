a16cc22e0f6ac2a3d88eb4d1ec7e62fd393c04cf
fabiohenrique86 <fabio.henrique@robbu.global>
2023-08-01 11:21:25 -0300

    1. added one Summary

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hell", "Beachy"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hell", "Beachy", "Housy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
